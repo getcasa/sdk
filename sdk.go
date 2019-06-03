6ee4159dfc5da51996cdcb12a504e3b8c36d11f0
Jimi M <jimi.maiquez@gmail.com>
2019-06-03 17:02:58 +0200

    add interface

--- sdk.go
+++ sdk.go
@@ -2,10 +2,14 @@ package sdk
 
 import "fmt"
 
-// PrintLogType define PrintLog function
-type PrintLogType func(string)
+type Context interface {
+	PrintLog(string)
+}
+
+type context struct {
+}
 
 // PrintLog print log to casa gateway
-func PrintLog(str string) {
+func (c *context) PrintLog(str string) {
 	fmt.Println(str)
 }
